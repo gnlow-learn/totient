96ad083551bc6479d24073d8e0647b44d4ad1284
Gnlow <gnlow@pm.me>
2025-11-26 15:08:25 +0900

    init

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # totient
+```ts
+import { jordan, euler } from "https://esm.sh/gh/gnlow-learn/totient/mod.ts"
 
+console.log(jordan(3)(6)) // 182
+console.log(euler(123)) // 80
+```
---
+++ mod.ts
@@ -0,0 +1,20 @@
+const primes = [
+  2, 3, 5, 7, 11, 13, 17, 19, 23, 
+  29, 31, 37, 41, 43, 47, 53, 59, 
+  61, 67, 71, 73, 79, 83, 89, 97,
+]
+
+const factors =
+(n: number) => {
+    if (Math.sqrt(n) > primes.at(-1)!) {
+        throw ""
+    }
+    return primes.filter(x => n % x == 0)
+}
+
+export const jordan =
+(k: number) =>
+(n: number) =>
+    factors(n).reduce((a, b) => a-a/b**k, n**k)
+
+export const euler = jordan(1)
